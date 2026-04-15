515c2154155c381a813364a0c946681ae05a09f5
digant73 <diguardo@tiscali.it>
2026-04-15 20:21:40 +0200

    update

--- rpcs3/rpcs3qt/game_list_actions.cpp
+++ rpcs3/rpcs3qt/game_list_actions.cpp
@@ -373,7 +373,7 @@ void game_list_actions::ShowGameIntegrityDialog(const game_info& game)
 		return;
 
 	progress_dialog* pdlg = new progress_dialog(tr("ISO File Hash Calculation"), tr("Calculating hash"), tr("Cancel"),
-		0, 100, true, m_game_list_frame);
+		0, 100, false, m_game_list_frame);
 
 	pdlg->setAutoClose(false);
 	pdlg->setAutoReset(false);
@@ -391,6 +391,8 @@ void game_list_actions::ShowGameIntegrityDialog(const game_info& game)
 	// so run it on a concurrent thread avoiding to block the entire GUI
 	m_game_integrity_future = QtConcurrent::run([this]()
 	{
+		thread_base::set_name("Game Integrity");
+
 		QString text;
 		std::string hash, game_name;
 		bool info_dialog = false;
@@ -403,9 +405,7 @@ void game_list_actions::ShowGameIntegrityDialog(const game_info& game)
 		{
 			text = "Integrity check completed!\n\n";
 
-			const QString db_path = m_gui_settings->GetSettingsDir() + QString::fromStdString(rpcs3::utils::get_redump_db_filename());
-
-			switch (m_iso_validator->check_integrity(m_iso_validator->get_path(), db_path.toStdString(), hash, &game_name))
+			switch (m_iso_validator->check_integrity(m_iso_validator->get_path(), hash, &game_name))
 			{
 			case iso_integrity_status::NO_MATCH:
 				text += tr("Game check NOT PASSED\n\nNo match found on DB or game corrupted:\n - Hash: %0")
@@ -442,6 +442,8 @@ void game_list_actions::ShowGameIntegrityDialog(const game_info& game)
 	// Thread responsible to update the progress bar and to make cleanup when the hash calculation terminates
 	QtConcurrent::run([this, pdlg]()
 	{
+		thread_base::set_name("Game Integrity Progress");
+
 		while (m_iso_validator->get_status() == iso_hash_status::INITIALIZED)
 		{
 			// Set progress in range 0-100
@@ -449,12 +451,20 @@ void game_list_actions::ShowGameIntegrityDialog(const game_info& game)
 				(static_cast<float>(m_iso_validator->get_bytes_read()) / m_iso_validator->get_size()) * 100 :
 				0;
 
-			pdlg->setValue(progress);
+			Emu.CallFromMainThread([this, pdlg, progress]()
+			{
+				pdlg->setValue(progress);
+			}, nullptr, false);
+
 			std::this_thread::sleep_for(1000ms); // Wait for a little while
 		}
 
-		// As last, close the progress bar (object was also configured to be automatically deleted on close)
-		pdlg->close();
+		Emu.CallFromMainThread([this, pdlg]()
+		{
+			// As last, close the progress bar (it will be already closed if the process was aborted) and delete the object
+			pdlg->close();
+			delete pdlg;
+		}, nullptr, false);
 	});
 }
 
